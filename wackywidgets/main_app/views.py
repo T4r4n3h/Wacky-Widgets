9e6b57ab0d2fb5cadd4602d367047f7f3d26e10d
Taraneh Saleh <taranehsaleh@Taranehs-MacBook-Pro.local>
2021-10-29 16:37:04 -0400

    basic done

--- wackywidgets/main_app/views.py
+++ wackywidgets/main_app/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Widget
-
+from .forms import WidgetForm
+from django.db.models import Sum
 # Create your views here.
 
 def home(request):
     widget= Widget.objects.all()
+    widget_form = WidgetForm()
     return render(request,'index.html',{
-      'widget':widget
+      'widget':widget,
+      'widget_form':widget_form
   })
+
+def  add(request):
+    form = WidgetForm(request.POST)
+    new_widget = form.save()
+    # total = Widget.objects.annotate(Sum('quanitity'))
+    return redirect('home')
+
+
+
+
+def delete(request,wdg_id):
+    del_widget = Widget.objects.get(id=wdg_id)
+    print(f'this is the one we want to DELETE,{del_widget}')
+    del_widget.delete()
+    return redirect('home')
